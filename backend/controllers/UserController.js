ad29879fb295fc647f970d2325348fd735092396
leonardo <leopereirasilva86@gmail.com>
2024-04-15 21:29:42 -0300

    Criando atualização de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,8 @@ const User = require("../models/User")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
+const mongoose = require("mongoose")
+
 const jwtSecret = process.env.JWT_SECRET;
 
 const generateToken = (id) => {
@@ -78,8 +80,46 @@ const getCurrentUser = async(req, res) => {
     res.status(200).json(user);
 }
 
+// Update an user
+const update = async(req, res) => {
+    const{ name, password, bio} = req.body
+
+    let profileImage = null
+    if(res.file){
+        profileImage = req.file.fileName
+    }
+
+    const reqUser = req.user
+
+    const user = await User.findById(reqUser._id).select("-password")
+
+    if(name){
+        user.name = name
+    }
+
+    if(password){
+        // Generate password hash
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        user.password = passwordHash
+    }
+
+    if(profileImage){
+        user.profileImage = profileImage
+    }
+
+    if(bio){
+        user.bio = bio
+    }
+
+    await user.save();
+    res.status(200).json(user);
+}
+
 module.exports = {
     register,
     login,
     getCurrentUser,
+    update
 }
