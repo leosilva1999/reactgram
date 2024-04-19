b2cf5807cd9e4be1ffa3f21378e54c8132e88358
leonardo <leopereirasilva86@gmail.com>
2024-04-18 22:40:22 -0300

    insert photos created

--- backend/middlewares/photoValidation.js
+++ backend/middlewares/photoValidation.js
@@ -1,15 +1,17 @@
 const {body} = require("express-validator")
 
 const photoInsertValidation = () => {
+    
     return[
         body("title")
         .not()
         .equals("undefined")
         .withMessage("O título é obrigatório")
         .isString()
+        .withMessage("O título é obrigatório")
         .isLength({min: 3})
         .withMessage("O título precisa ter no mínimo 3 caracteres."),
-        body("image").custom((value, [req]) => {
+        body("image").custom((value, {req}) => {
             if(!req.file){
                 throw new Error("A imagem é obrigatória.")
             }
@@ -17,6 +19,7 @@ const photoInsertValidation = () => {
         })
     ]
 }
+
 
 module.exports = {
     photoInsertValidation,
